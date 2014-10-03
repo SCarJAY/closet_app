28cf575f2e106ab7a1cec5d8f682fb77f890e408
psweetpotato <p.sweetpotato@gmail.com>
2014-10-03 18:03:02 -0400

    adds tests for relationship with items and outfits and secure password test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe User, :type => :model do
   it { should validate_presence_of(:password_digest) }
   it { should validate_presence_of(:city) }
   it { should validate_presence_of(:state) }
+  it { should have_many(:items) }
+  it { should have_many(:outfits) }
+  it { should have_secure_password(:password_digest) }
 end
