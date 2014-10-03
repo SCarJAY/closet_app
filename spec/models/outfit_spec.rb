13e21cac46e014576a9658b2ab252fd0bf56fac0
psweetpotato <p.sweetpotato@gmail.com>
2014-10-03 18:23:41 -0400

    adds relationship tests

--- spec/models/outfit_spec.rb
+++ spec/models/outfit_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Outfit, :type => :model do
   it { should validate_presence_of(:name) }
+  it { should have_many(:items) }
+  it { should belong_to(:users) }
 end
