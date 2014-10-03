086c32445d4095236706785dc7efbceeffdc2472
psweetpotato <p.sweetpotato@gmail.com>
2014-10-03 18:24:19 -0400

    adds relationship tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it { should validate_presence_of(:name) }
+  it { should ensure_length_of(:name, is_at_least(2), is_at_most(20))}
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password_digest) }
