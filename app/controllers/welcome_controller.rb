4dbe9dc18ac10f08959a4b2d15191156193b89c6
psweetpotato <p.sweetpotato@gmail.com>
2014-10-03 17:32:29 -0400

    adds welcome controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  def index
+    if session[:user_id]
+      user = User.find_by(session[:user_id])
+      redirect_to(user_path(user))
+    else
+      redirect_to(login_path)
+    end
+  end
+end
