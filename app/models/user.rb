6c0267d8669f1d1895dd02d5ed0cce1f38f9cd56
psweetpotato <p.sweetpotato@gmail.com>
2014-10-04 12:57:33 -0400

    adds password presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :items
   has_many :outfits
-  validates :name, :email, :city, :state, presence: true
+  validates :name, :email, :city, :state, :password_digest, presence: true
   validates :email, uniqueness: true
   has_secure_password
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it { should validate_presence_of(:name) }
-  it { should ensure_length_of(:name, is_at_least(2), is_at_most(20))}
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password_digest) }
@@ -10,5 +9,5 @@ RSpec.describe User, :type => :model do
   it { should validate_presence_of(:state) }
   it { should have_many(:items) }
   it { should have_many(:outfits) }
-  it { should have_secure_password(:password_digest) }
+  it { should have_secure_password }
 end
