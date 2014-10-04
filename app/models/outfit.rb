1373b5e00431ae22c6272b28140768e95a58e0cc
psweetpotato <p.sweetpotato@gmail.com>
2014-10-04 11:02:21 -0400

    updates spec to include through association

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,4 +1,5 @@
 class Outfit < ActiveRecord::Base
   belongs_to :user
   has_many :items, through: :cameos
+  validates_presence_of :name
 end
--- spec/models/outfit_spec.rb
+++ spec/models/outfit_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Outfit, :type => :model do
   it { should validate_presence_of(:name) }
-  it { should have_many(:items) }
+  it { should have_many(:items).through(:cameos) }
   it { should belong_to(:users) }
 end
