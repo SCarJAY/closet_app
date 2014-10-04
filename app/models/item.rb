24e195d2380a11671a6f0665e89a0c64fb722ecc
psweetpotato <p.sweetpotato@gmail.com>
2014-10-04 11:24:23 -0400

    changes model to validate presence of not null columns

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+  has_many :cameos
   has_many :outfits, through: :cameos
+  validates_presence_of :img_url, :name, :category, :dress_code, :color, :size
 end
