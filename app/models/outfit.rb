86977110e7da180fa68025e2e4d9fa4044a9fccd
psweetpotato <p.sweetpotato@gmail.com>
2014-10-05 14:53:09 -0400

    moves generate method into controller and uses sessions

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -15,19 +15,4 @@ class Outfit < ActiveRecord::Base
   has_many :items, through: :cameos
   validates_presence_of :name
 
-
-  def self.generate
-    outfit = {}
-    random = rand(1..2)
-    if random == 1
-      outfit["top"] = @user.items.where(category: "top").sample
-      outfit["bottom"] = @user.items.where(category: "bottom").sample
-    else
-      outfit["one_piece"] = @user.items.where(category: "one piece").sample
-    end
-    outfit["accessory"] = @user.items.where(category: "accessory").sample
-    outfit["shoes"] = @user.items.where(category: "shoes").sample
-    outfit
-  end
-
 end
