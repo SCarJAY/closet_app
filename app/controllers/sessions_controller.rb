964c03ed9f5b47a20f8086911217d531b671912a
carriexu <carrielovesfood@gmail.com>
2014-10-05 16:49:56 -0400

    added logout link on dashboard and items pages, also redirected root back to welcome page instead of login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+
     if user && user.authenticate(params[:password])
       log_in(user)
       redirect_to(dashboard_path(user))
