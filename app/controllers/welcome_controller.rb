4d666a73be7ed04d9e03e268f916873dec485390
adesuwau <missadeugbogbo@gmail.com>
2014-10-05 14:18:43 -0400

    Adds initial splash page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,9 @@ class WelcomeController < ApplicationController
   def index
     if session[:user_id]
       user = User.find_by(id: session[:user_id])
-      redirect_to(dashboard_path(id: @user.id))
-    else
-      redirect_to(login_path)
+      redirect_to(user_path(user))
+    # else
+    #   redirect_to(login_path)
     end
   end
 
