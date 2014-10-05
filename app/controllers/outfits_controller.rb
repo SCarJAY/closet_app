61873eb70fff690585da6d46a6abd9c6149b6b7a
yinansong <yinan@yinansong.com>
2014-10-05 16:59:20 -0400

    outfit index page (basic version is working)

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,10 +1,26 @@
 class OutfitsController < ApplicationController
   before_action :set_outfit, only: [:show, :edit, :update, :destroy]
 
+  def generate
+      @user = User.find_by(id: session[:user_id])
+      @outfit = {}
+      random = rand(1..2)
+      if random == 1
+        @outfit["top"] = @user.items.where(category: "top").sample
+        @outfit["bottom"] = @user.items.where(category: "bottom").sample
+      else
+        @outfit["one_piece"] = @user.items.where(category: "one piece").sample
+      end
+      @outfit["accessory"] = @user.items.where(category: "accessory").sample
+      @outfit["shoes"] = @user.items.where(category: "shoes").sample
+      @outfit
+  end
+
   # GET /outfits
   # GET /outfits.json
   def index
-    @outfits = Outfit.all
+    @user = User.find_by(id: session[:user_id])
+    @outfits = @user.outfits
   end
 
   # GET /outfits/1
