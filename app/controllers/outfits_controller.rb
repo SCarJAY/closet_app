1c21b71efd89846a4089c3afc9030650780574bc
yinansong <yinan@yinansong.com>
2014-10-06 07:12:22 -0400

    get the cameo routes to work; get the generated outfit to be samved

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -8,20 +8,24 @@ class OutfitsController < ApplicationController
     @dress_codes = @user.items.map { |item| item[:dress_code] }
     @dress_codes_uniq = @dress_codes.uniq
     @selected_dress_code = params[:dress_code]
+    # binding.pry
 
-    @new_outfit = {}
     random = rand(1..2)
     if random == 1
-      @new_outfit["top"] = @user.items.where(dress_code: @selected_dress_code).where(category: "top").sample
-      @new_outfit["bottom"] = @user.items.where(dress_code: @selected_dress_code).where(category: "bottom").sample
+      @new_outfit_top = @user.items.where(dress_code: @selected_dress_code).where(category: "top").sample
+      @new_outfit_bottom = @user.items.where(dress_code: @selected_dress_code).where(category: "bottom").sample
+      # binding.pry
     else
-      @new_outfit["one_piece"] = @user.items.where(dress_code: @selected_dress_code).where(category: "one piece").sample
+      @new_outfit_onepiece = @user.items.where(dress_code: @selected_dress_code).where(category: "one piece").sample
     end
-    @new_outfit["accessory"] = @user.items.where(dress_code: @selected_dress_code).where(category: "accessory").sample
-    @new_outfit["shoes"] = @user.items.where(dress_code: @selected_dress_code).where(category: "shoes").sample
-    # @new_outfit = {"top": certain_item1, "bottom": certain_item2, "accessory": certain_item3, "shoes": certain_item4}
-    @new_outfit
-    # Cameo.new(outfit_id: @new_outfit["top"].item_id)
+    @new_outfit_accessory = @user.items.where(dress_code: @selected_dress_code).where(category: "accessory").sample
+    @new_outfit_shoes = @user.items.where(dress_code: @selected_dress_code).where(category: "shoes").sample
+    Cameo.create(item_id: @new_outfit_onepiece.id) if @new_outfit_onepiece
+    Cameo.create(item_id: @new_outfit_top.id) if @new_outfit_top
+    Cameo.create(item_id: @new_outfit_bottom.id) if @new_outfit_bottom
+    Cameo.create(item_id: @new_outfit_accessory.id) if @new_outfit_accessory
+    Cameo.create(item_id: @new_outfit_shoes.id) if @new_outfit_shoes
+    # binding.pry
   end
 
   # POST /outfits
@@ -40,6 +44,8 @@ class OutfitsController < ApplicationController
         format.json { render json: @outfit.errors, status: :unprocessable_entity }
       end
     end
+    # set all the stuff in cameo where the the outfit_id = nil to be this new id
+    Cameo.all.where(outfit_id: nil).update_all("outfit_id" => @outfit.id)
   end
 
   # GET /outfits
@@ -54,7 +60,6 @@ class OutfitsController < ApplicationController
   def show
     @user = User.find_by(id: params[:user_id])
     @outfit = @user.outfits.find(params[:id])
-    binding.pry
   end
 
   # GET /outfits/new
@@ -84,7 +89,8 @@ class OutfitsController < ApplicationController
   # DELETE /outfits/1
   # DELETE /outfits/1.json
   def destroy
-    @outfit = @user.outfits.find(params[:id])
+    @outfit = Outfit.find(params[:id])
+    Cameo.all.where(outfit_id: @outfit.id).destroy_all
     @outfit.delete
     respond_to do |format|
       format.html { redirect_to user_outfits_path, notice: 'Outfit was successfully destroyed.' }
